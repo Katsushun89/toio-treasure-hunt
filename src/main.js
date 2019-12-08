f5f643a73a21dc95261c871f70de9dc4366bb46b
s-katsu <katsumata@whill.jp>
2019-12-09 02:04:05 +0900

    Add hint3.

--- src/main.js
+++ src/main.js
@@ -5,30 +5,83 @@ let cube = null;
 
 var is_got_hint1 = false;
 var is_got_hint2 = false;
-var hint1_text = 'レゴの2x3ブロックの中';
-var hint2_text = 'お風呂の浴槽';
+var is_got_hint3 = false;
+var hint1_text = 'レゴの2x3ブロックが入っているケースの中';
+var hint2_text = 'マットの上でtoioを時計回りに1回,反時計回りに2回回転させろ';
+var hint3_text = '最後のカギはドラゴンボール34巻に挟まれている';
+
+var is_set_initial_angle = false;
+var last_angle;
+var last_angle2;
+var clockwise_cnt = 0;
+var counter_clockwise_cnt = 0;
 
 document.getElementById('connect').addEventListener('click', async () => {
   cube = await new NearestScanner().start();
   document.body.className = 'cube-connecting';
 
   await cube.connect();
-//  cube.on('id:position-id', info => (document.getElementById('position-id').innerHTML = JSON.stringify(info)));
-//  cube.on('id:position-id-missed', () => (document.getElementById('position-id').innerHTML = ''));
   cube.on('id:standard-id', info => {
     document.getElementById('standard-id').innerHTML = JSON.stringify(info);
     var hint1_content = '';
     var hint2_content = '';
 
     var id = info.standardId;
-    if(id == '3670054' || is_got_hint1){ hint1_content = hint1_text; is_got_hint1 = true;}
-    if(id == '3670022' || is_got_hint2){ hint2_content = hint2_text; is_got_hint2 = true;}
+
+    if(id == '3670054'|| is_got_hint1){ hint1_content = hint1_text; is_got_hint1 = true;}
+    if(is_got_hint1){
+      if(id == '3670022'|| is_got_hint2){ hint2_content = hint2_text; is_got_hint2 = true;}
+    }
+
     document.getElementById('hint1').innerHTML = hint1_content;
     document.getElementById('hint2').innerHTML = hint2_content;
 
   });
   
-  cube.on('id:standard-id-missed', () => (document.getElementById('standard-id').innerHTML = ''));
+  cube.on('id:standard-id-missed', () => {
+    document.getElementById('standard-id').innerHTML = '';
+  });
+  
+  cube.on('id:position-id', info => {
+    document.getElementById('position-id').innerHTML = JSON.stringify(info);
+
+    var hint3_content = '';
+
+    var angle = info.angle;
+    if(is_got_hint2){
+      if(!is_set_initial_angle){
+        is_set_initial_angle = true;
+        clockwise_cnt = 0;
+        counter_clockwise_cnt = 0;
+        last_angle = angle;
+        last_angle2 = angle;
+      }else{
+        var diff = angle - last_angle;
+        var diff2 = last_angle - last_angle2;
+        if(Math.abs(diff) > 3 && Math.abs(diff2) > 3){
+          if(diff2 > 0 && diff < 0 && angle < 50 && last_angle > 310){clockwise_cnt++;}
+          if(diff2 < 0 && diff > 0 && angle > 310 && last_angle < 50){counter_clockwise_cnt++;}
+        }
+
+        last_angle2 = last_angle;
+        last_angle = angle;
+      }
+    }
+
+    document.getElementById('clockwise-cnt').innerHTML = clockwise_cnt;
+    document.getElementById('counter-clockwise-cnt').innerHTML = counter_clockwise_cnt;
+
+    if(clockwise_cnt >=1 && counter_clockwise_cnt >= 2 || is_got_hint3){
+      is_got_hint3 = true;
+      hint3_content = hint3_text;
+    }
+    document.getElementById('hint3').innerHTML = hint3_content;
+  });
+ 
+  cube.on('id:position-id-missed', () => {
+    document.getElementById('position-id').innerHTML = '';
+    is_set_initial_angle = false;
+  });
 
   document.body.className = 'cube-connected';
 });
